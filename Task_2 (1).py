1a6c10e2f3598ed96a5d6feb4d3090879259726a
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-12-04 18:50:52 +0530

    Slicing

---
+++ Task_2 (1).py
@@ -0,0 +1,6 @@
+A = "AB2CD3EF4GH5IJ6KL7MN8OP9QR1ST2UV3WX4YZ"
+print(A[2::3])
+print(A[2::3][0:5:2])
+print(type(A[2::3][0:5:2]))
+print(int(A[2::3][0:5:2]))
+print(bin(int(A[2::3][0:5:2])))
---
+++ Task_3 (1).py
@@ -0,0 +1,5 @@
+A = "1A2B3C4D5E6F7G8H9I1J2K3L4M5N6O7P8Q9R1S2T3U4V5W6X7Y8Z"
+print(A[0::3])
+print(A[0::3][8:15:2])
+print(int(A[0::3][8:15:2]))
+print(oct(int(A[0::3][8:15:2])))
